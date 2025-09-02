72e5bd89f4746a8d5c96b7b73e2ef571d094db69
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2025-09-02 22:03:39 +0600

    ADD: system component diagram added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler } from 'express';
 import { IGenericErrorMessage } from '../../types';
 import handleValidationError from '../../errors/handleValidationError';
